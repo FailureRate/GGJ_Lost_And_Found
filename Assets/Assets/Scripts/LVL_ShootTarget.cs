1077d4b3a47eeed7e49ffab432dce0f7b2537f00
Raphael Rodriguez <32069809+FailureRate@users.noreply.github.com>
2021-01-31 12:44:38 -0500

    Rooms 1 -4 Done

--- Assets/Assets/Scripts/LVL_ShootTarget.cs
+++ Assets/Assets/Scripts/LVL_ShootTarget.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class LVL_ShootTarget : MonoBehaviour
 {
     [Header("Activatable")]
-    [SerializeField] private GameObject Interactable;
+    [SerializeField] private GameObject[] Interactable;
 
     [Header("Material")]
     [SerializeField] private Material activatedMat;
@@ -36,13 +36,34 @@ public class LVL_ShootTarget : MonoBehaviour
     {
         if (other.gameObject.tag == "Bullet" && isSwitchActivated == false)
         {
-            Interactable.GetComponent<LVL_Door>().Activate();
+            foreach(GameObject doors in Interactable)
+            {
+                if (doors.GetComponent<LVL_Door>().isOpen)
+                {
+                    doors.GetComponent<LVL_Door>().Deactivate();
+                }
+                else
+                {
+                    doors.GetComponent<LVL_Door>().Activate();
+                }
+            }
+            //Interactable.GetComponent<LVL_Door>().Activate();
             Crystal.GetComponent<MeshRenderer>().material = activatedMat;
             isSwitchActivated = true;
         }
 
         else if (other.gameObject.tag == "Bullet" && isSwitchActivated == true) {
-            Interactable.GetComponent<LVL_Door>().Deactivate();
+            foreach (GameObject doors in Interactable)
+            {
+                if (doors.GetComponent<LVL_Door>().isOpen)
+                {
+                    doors.GetComponent<LVL_Door>().Deactivate();
+                }
+                else
+                {
+                    doors.GetComponent<LVL_Door>().Activate();
+                }
+            }
             Crystal.GetComponent<MeshRenderer>().material = deactivatedMat;
             isSwitchActivated = false;
         }
