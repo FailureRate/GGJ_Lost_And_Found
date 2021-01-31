97a0676a2b7ec68edaa660afd7bbe1812fd49658
Raphael Rodriguez <32069809+FailureRate@users.noreply.github.com>
2021-01-31 13:15:30 -0500

    Update LVL_ShootTarget.cs

--- Assets/Assets/Scripts/LVL_ShootTarget.cs
+++ Assets/Assets/Scripts/LVL_ShootTarget.cs
@@ -4,83 +4,84 @@ using UnityEngine;
 
 public class LVL_ShootTarget : MonoBehaviour
 {
-    [Header("Activatable")]
-    [SerializeField] private GameObject[] Interactable;
+    //[Header("Activatable")]
+    //[SerializeField] private GameObject[] Interactable;
 
-    [Header("Material")]
-    [SerializeField] private Material activatedMat;
-    [SerializeField] private Material deactivatedMat;
 
-    [Header("Reference")]
-    [SerializeField] private GameObject Crystal;
+    //[Header("Material")]
+    //[SerializeField] private Material activatedMat;
+    //[SerializeField] private Material deactivatedMat;
 
-    [SerializeField] private bool isSwitchActivated;
-    public LVL_Door DoorScript;
+    //[Header("Reference")]
+    //[SerializeField] private GameObject Crystal;
 
-    private void Update()
-    {
-        if(Interactable.Length > 1)
-        {
-            if (isSwitchActivated)
-            {
-                Crystal.GetComponent<MeshRenderer>().material = activatedMat;
-            }
-            else
-            {
-                Crystal.GetComponent<MeshRenderer>().material = deactivatedMat;
-            }
-        }
-        else
-        {
-            if (DoorScript.isOpen == true)
-            {
-                Crystal.GetComponent<MeshRenderer>().material = activatedMat;
-                isSwitchActivated = true;
-            }
+    //[SerializeField] private bool isSwitchActivated;
+    //public LVL_Door DoorScript;
 
-            else if (DoorScript.isOpen == false)
-            {
-                Crystal.GetComponent<MeshRenderer>().material = deactivatedMat;
-                isSwitchActivated = false;
-            }
-        }
+    //private void Update()
+    //{
+    //    if(Interactable.Length > 1)
+    //    {
+    //        if (isSwitchActivated)
+    //        {
+    //            Crystal.GetComponent<MeshRenderer>().material = activatedMat;
+    //        }
+    //        else
+    //        {
+    //            Crystal.GetComponent<MeshRenderer>().material = deactivatedMat;
+    //        }
+    //    }
+    //    else
+    //    {
+    //        if (DoorScript.isOpen == true)
+    //        {
+    //            Crystal.GetComponent<MeshRenderer>().material = activatedMat;
+    //            isSwitchActivated = true;
+    //        }
+
+    //        else if (DoorScript.isOpen == false)
+    //        {
+    //            Crystal.GetComponent<MeshRenderer>().material = deactivatedMat;
+    //            isSwitchActivated = false;
+    //        }
+    //    }
   
-    }
+    //}
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "Bullet" && isSwitchActivated == false)
-        {
-            foreach(GameObject doors in Interactable)
-            {
-                if (doors.GetComponent<LVL_Door>().isOpen)
-                {
-                    doors.GetComponent<LVL_Door>().Deactivate();
-                }
-                else
-                {
-                    doors.GetComponent<LVL_Door>().Activate();
-                }
-            }
-            //Interactable.GetComponent<LVL_Door>().Activate();
-            Crystal.GetComponent<MeshRenderer>().material = activatedMat;
-            isSwitchActivated = true;
-        }
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.tag == "Bullet" && isSwitchActivated == false)
+    //    {
+    //        foreach(GameObject doors in Interactable)
+    //        {
+    //            if (doors.GetComponent<LVL_Door>().isOpen)
+    //            {
+    //                doors.GetComponent<LVL_Door>().Deactivate();
+    //            }
+    //            else
+    //            {
+    //                doors.GetComponent<LVL_Door>().Activate();
+    //            }
+    //        }
+    //        //Interactable.GetComponent<LVL_Door>().Activate();
+    //        Crystal.GetComponent<MeshRenderer>().material = activatedMat;
+    //        isSwitchActivated = true;
+    //    }
 
-        else if (other.gameObject.tag == "Bullet" && isSwitchActivated == true) {
-            foreach (GameObject doors in Interactable)
-            {
-                if (doors.GetComponent<LVL_Door>().isOpen)
-                {
-                    doors.GetComponent<LVL_Door>().Deactivate();
-                }
-                else
-                {
-                    doors.GetComponent<LVL_Door>().Activate();
-                }
-            }
-            Crystal.GetComponent<MeshRenderer>().material = deactivatedMat;
-            isSwitchActivated = false;
-        }
-    }
+    //    else if (other.gameObject.tag == "Bullet" && isSwitchActivated == true) {
+    //        foreach (GameObject doors in Interactable)
+    //        {
+    //            if (doors.GetComponent<LVL_Door>().isOpen)
+    //            {
+    //                doors.GetComponent<LVL_Door>().Deactivate();
+    //            }
+    //            else
+    //            {
+    //                doors.GetComponent<LVL_Door>().Activate();
+    //            }
+    //        }
+    //        Crystal.GetComponent<MeshRenderer>().material = deactivatedMat;
+    //        isSwitchActivated = false;
+    //    }
+    //}
 }
